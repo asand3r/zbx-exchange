be88d290c30ffc98ad9b39d2036b5d4ae86596ed
Khatsayuk Alexander <asand3r@gmail.com>
2019-06-06 14:40:40 +0300

    new file:   zbx-exchange.conf 	new file:   zbx-exchange.ps1 Initial commit.

---
+++ zbx-exchange.ps1
@@ -0,0 +1,18 @@
+﻿# Trying add EMS snapin
+# Exchange 2010: Microsoft.Exchange.Management.PowerShell.E2010
+# Exchange 2013/2016: Microsoft.Exchange.Management.PowerShell.SnapIn
+try {
+    Add-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.SnapIn -ErrorAction Stop
+} catch [Exception] {
+    return -1
+}
+
+# Low-Level Discovery function
+function Make-LLD() {
+    $dbs = Get-MailboxDatabase | Select-Object @{Name = "{#DB.NAME}"; e={$_.Name}},
+                                  @{Name = "{#DB.LCNAME}"; e={$_.Name.tolower()}},
+                                  @{Name = "{#DB.EDBPATH}"; e={$_.EdbFilePath}}
+    return ConvertTo-Json @{"data" = [array]$dbs} -Compress
+}
+
+Write-Host $(Make-LLD)
